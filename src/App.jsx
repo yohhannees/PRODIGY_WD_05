bb75af85a8d1c9ca093bbe604d69f1e57214d15e
Yohannees <yohannesgetachewerieso@gmail.com>
2023-09-07 00:31:08 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 
-import  { useEffect, useState } from "react";
-export default function App() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
-  }, [lat, long]);
-
-  return <div className="App"></div>;
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
+
+export default App
